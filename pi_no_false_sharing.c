3fa85190adc2947393472399f7ef4dbc095e092a
gbertini <gcalbertini@gmail.com>
2022-03-17 13:41:25 -0700

    File updates to pi

--- pi_no_false_sharing.c
+++ pi_no_false_sharing.c
@@ -13,17 +13,21 @@ from the OpenMP runtime library
 History: Written by Tim Mattson, 11/99.
 
 */
-//gcc -fopenmp pi.c && ./a.out
+//gcc -fopenmp pi_no_false_sharing.c && ./a.out
 #include <stdio.h>
 #include <omp.h>
 #define NUM_THREADS 4
+// In this version we set pad to be number of double precision words in cache line;
+// First element is id and second element always 0 so now guarantee that every
+// lement of array sits on different cache line!
+# define PAD 8 // assume 64 byte L1 cache line size
 static long num_steps = 100000000;
 double step;
 int main ()
 {
 	  int i, nthreads;
 	  double x, pi;
-	  double start_time, run_time, sum[NUM_THREADS];
+	  double start_time, run_time, sum[NUM_THREADS][PAD];
 	  step = 1.0/(double) num_steps;
 	  omp_set_num_threads(NUM_THREADS);
 	  #pragma omp parallel
@@ -41,15 +45,15 @@ int main ()
 		  if (id == 0) nthreads = nthrds;
 
 		  start_time = omp_get_wtime();
-		  for (i = id, sum[id] = 0.0; i< num_steps; i+=nthrds){
+		  for (i = id, sum[id][0] = 0.0; i< num_steps; i+=nthrds){
 			  x = (i+0.5)*step;
-			  sum[id]+=4.0/(1.0+x*x);
+			  sum[id][0]+=4.0/(1.0+x*x);
 		  }
 	  }
 	  // Now all vars from thread stack are lost (aside from master threads')
 	  // aside from sum which is shared to avoid race conditions
 	  for (i = 0, pi = 0.0; i < nthreads; ++i)
-	  	pi += sum[i]*step;
+	  	pi += sum[i][0]*step;
 	  run_time = omp_get_wtime() - start_time;
 	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
 }	  
