ac5f9f23bd9a91cb7cb2e48983107a2d6892c60f
Guilherme Albertini <gcalbertini@gmail.com>
2022-03-17 13:07:18 -0700

    pi.c update

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <omp.h>
+//gcc -fopenmp hello.c && ./a.out
 int main ()  
 {
-  printf("Hello World \n");
+  #pragma omp parallel
+  {
+    int ID = omp_get_thread_num();
+    printf("Hello(%d)",ID);
+    printf("World(%d)\n",ID);
+  }
 }
--- pi.c
+++ pi.c
@@ -13,27 +13,35 @@ from the OpenMP runtime library
 History: Written by Tim Mattson, 11/99.
 
 */
+//gcc -fopenmp pi.c && ./a.out
 #include <stdio.h>
 #include <omp.h>
+#define NUM_THREADS 2
 static long num_steps = 100000000;
 double step;
 int main ()
 {
-	  int i;
-	  double x, pi, sum = 0.0;
-	  double start_time, run_time;
-
+	  int i, nthreads;
+	  double x, pi;
+	  double start_time, run_time, sum[NUM_THREADS];
 	  step = 1.0/(double) num_steps;
+	  omp_set_num_threads(NUM_THREADS);
+	  #pragma omp parallel
+	  {
+		  int i, id, nthrds;
+		  double x;
+		  id = omp_get_thread_num();
+		  nthrds = omp_get_num_threads();
+		  if (id == 0) nthreads = nthrds;
 
-        	 
-	  start_time = omp_get_wtime();
-
-	  for (i=1;i<= num_steps; i++){
-		  x = (i-0.5)*step;
-		  sum = sum + 4.0/(1.0+x*x);
+		  start_time = omp_get_wtime();
+		  for (i = id, sum[id] = 0.0; i< num_steps; i+=nthrds){
+			  x = (i+0.5)*step;
+			  sum[id]+=4.0/(1.0+x*x);
+		  }
 	  }
-
-	  pi = step * sum;
+	  for (i = 0, pi = 0.0; i < nthreads; ++i)
+	  	pi += sum[i]*step;
 	  run_time = omp_get_wtime() - start_time;
 	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
 }	  
